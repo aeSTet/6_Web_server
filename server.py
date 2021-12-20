d5a8045a76bdf93d573a40f703006273d153ca29
aeSTet <76133815+aeSTet@users.noreply.github.com>
2021-12-20 17:43:30 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,33 +1,122 @@
+import datetime
 import socket
-from threading import Thread
-import time
-def generate_function():
-    sock = socket.socket()
-    sock.bind(('', 80))
-    print("Сервер запущен")
-    sock.listen(5)
-    while True:
-        conn, addr = sock.accept()
-        thread = Thread(target=working, args=(conn,addr,))
-        thread.start()
-def working(conn, addr):
-    print("Присоединился", addr)
-    t = time.asctime(time.gmtime()).split(' ')
-    t = f'{t[0]}, {t[2]} {t[1]} {t[4]} {t[3]}'
-    print("Date: ",t)
-    h = f'HTTP/1.1 200 OK\nServer: SelfMadeServer v0.0.1\nDate: {t}\nContent-Type: text/html; charset=utf-8\nConnection: close\n\n'
-    user = conn.recv(1024).decode()
-    rez = user.split(" ")[1]
-    if rez=='/' or rez=='/index.html':
-        with open('index.html', 'rb') as f:
-            answer = f.read()
-            conn.send(h.encode('utf-8')+answer)
-    elif rez=="/1.html":
-         with open('1.html', 'rb') as f:
-            answer = f.read()
-            conn.send(h.encode('utf-8')+answer)
+import threading
+import logging
+from settings import *
+import os
+
+files_in_directory=os.listdir() #список файлов в директории
+
+
+def process(conn, addr):
+    print("Connected", addr)
+
+    data = conn.recv(REQUEST_LENGTH)
+    msg = data.decode()
+
+    print(msg)
+    msg_ = msg.split(' ')
+    file=msg_[1][1:]
+    answer=''
+    resp=''
+
+    if '.' in file:
+        if file in files_in_directory and file.split('.')[1] in ALLOWED_FORMATS:
+            with open(file, 'r', encoding="UTF-8") as f:
+                answer = f.read()
+            size = os.path.getsize(file)
+
+            resp = f"""HTTP/1.1 200 OK
+			Date: {get_date()}
+			Server: SelfMadeServer v0.0.1
+			Content-length: {size}
+			Content-type: text/html
+			Connection: close
+
+			{answer}
+			"""
+        elif file not in files_in_directory:
+            file="404.html"
+            with open(file, 'r', encoding="UTF-8") as f:
+                answer = f.read()
+            size = os.path.getsize(file)
+
+            resp = f"""HTTP/1.1 200 OK
+            Date: {get_date()}
+   			Server: SelfMadeServer v0.0.1
+   			Content-length: {size}
+			Content-type: text/html
+            Connection: close
+
+            {answer}
+            """
+        elif file.split('.')[1]  not in ALLOWED_FORMATS:
+            file = "403.html"
+            with open(file, 'r', encoding="UTF-8") as f:
+                answer = f.read()
+            size = os.path.getsize(file)
+
+            resp = f"""HTTP/1.1 200 OK
+            Date: {get_date()}
+            Server: SelfMadeServer v0.0.1
+            Content-length: {size}
+            Content-type: text/html
+            Connection: close
+
+            {answer}
+            """
+
     else:
-        resp = """HTTP/1.1 200 OK
-        NOT FOUND"""
-        conn.send(resp.encode('utf-8'))
-generate_function()
+        file = "404.html"
+        with open(file, 'r', encoding="UTF-8") as f:
+            answer = f.read()
+        size = os.path.getsize(file)
+
+        resp = f"""HTTP/1.1 200 OK
+        Date: {get_date()}
+        Server: SelfMadeServer v0.0.1
+        Content-length: {size}
+        Content-type: text/html
+        Connection: close
+
+        {answer}
+        """
+
+    try:
+        resp_ = resp.split()
+        error_number = resp_[1]
+        serv_log.info(f"{get_date()} {addr} {file} {error_number}")
+
+    except Exception as e:
+        pass
+
+
+    conn.send(resp.encode())
+
+    conn.close()
+
+
+def get_date():
+    return datetime.datetime.now()
+
+
+sock = socket.socket()
+
+try:
+    sock.bind(('', PORT))
+    print("Using port 80")
+except OSError:
+    sock.bind(('', 8080))
+    print("Using port 8080")
+
+sock.listen(5)
+
+serv_log = logging.getLogger('log_')
+serv_log_handler = logging.FileHandler('server.log', encoding='UTF-8')
+serv_log_handler.setLevel(logging.INFO)
+serv_log.addHandler(serv_log_handler)
+serv_log.setLevel(logging.INFO)
+
+while True:
+    conn, addr = sock.accept()
+    threading.Thread(target=process, args=[conn, addr]).start()
